a42276c46ed9ef5ee01883a4572df7504721df6a
wanghaiming <18801116541@163.com>
2016-06-27 14:02:16 +0800

    Signed-off-by: wanghaiming <18801116541@163.com>

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -19,6 +19,11 @@ class Index extends Controller
 		}
 		$education = array();
 		$associations = array();
+		$awards = array();
+		$work_experience = array();
+		$legal_cases = array();
+		$publications = array();
+		$speaking_engagements = array();
 		$search = $arr[3];
 		$cond = $arr[0];
 		$mongo = new Mongodb('avvo_lawyer_info','lawyers');
@@ -39,6 +44,11 @@ class Index extends Controller
 				$phone = $val['contact']['phone'];
 			}else{
 				$phone = "";
+			}
+			if(!empty($val['contact']['fax'])){
+				$fax = $val['contact']['fax'];
+			}else{
+				$fax = "";
 			}
 			if(!empty($val['contact']['address']['street_address'])){
 				$street = $val['contact']['address']['street_address'];
@@ -65,16 +75,54 @@ class Index extends Controller
 			}else{
 				$state = "";
 			}
-			foreach($val['education'] as $k => $v){
-				$education[$k]['graduated']  = $v['graduated'];
-				$education[$k]['major']  	 = $v['major'];
-				$education[$k]['school_name']= $v['school_name'];
-				$education[$k]['degree']  	 = $v['degree'];
+			if(!empty($val['education'])){
+				foreach($val['education'] as $k => $v){
+					$education[$k]['graduated']  = $v['graduated'];
+					$education[$k]['major']  	 = $v['major'];
+					$education[$k]['school_name']= $v['school_name'];
+					$education[$k]['degree']  	 = $v['degree'];
+				}
 			}
-			foreach($val['associations'] as $k => $v){
-				$associations[$k]['position_name'] = $v['position_name'];
-				$associations[$k]['association_name'] = $v['association_name'];
-				$associations[$k]['duration'] = $v['duration'];
+			if(!empty($val['associations'])){
+				foreach($val['associations'] as $k => $v){
+					$associations[$k]['position_name']    = $v['position_name'];
+					$associations[$k]['association_name'] = $v['association_name'];
+					$associations[$k]['duration'] 		  = $v['duration'];
+				}
+			}
+			if(!empty($val['awards'])){
+				foreach($val['awards'] as $k => $v){
+					$awards[$k]['award_name']   = $v['award_name'];
+					$awards[$k]['date_granted'] = $v['date_granted'];
+					$awards[$k]['grantor'] 	    = $v['grantor'];
+				}
+			}
+			if(!empty($val['work_experience'])){
+				foreach($val['work_experience'] as $k => $v){
+					$work_experience[$k]['duration']     = $v['duration'];
+					$work_experience[$k]['company_name'] = $v['company_name'];
+					$work_experience[$k]['title'] 	     = $v['title'];
+				}
+			}
+			if(!empty($val['legal_cases'])){
+				foreach($val['legal_cases'] as $k => $v){
+					$legal_cases[$k]['outcome']     = $v['outcome'];
+					$legal_cases[$k]['case_name'] = $v['case_name'];
+				}
+			}
+			if(!empty($val['publications'])){
+				foreach($val['publications'] as $k => $v){
+					$publications[$k]['date']     		  = $v['date'];
+					$publications[$k]['publication_name'] = $v['publication_name'];
+					$publications[$k]['title'] 			  = $v['title'];
+				}
+			}
+			if(!empty($val['speaking_engagements'])){
+				foreach($val['speaking_engagements'] as $k => $v){
+					$speaking_engagements[$k]['date']     		 = $v['date'];
+					$speaking_engagements[$k]['conference_name'] = $v['conference_name'];
+					$speaking_engagements[$k]['title'] 			 = $v['title'];
+				}
 			}
 			$practice = implode($val['practice areas'],',');
 			//var_dump($val);exit;
@@ -84,14 +132,34 @@ class Index extends Controller
 			$this->assign('city',$city);
 			$this->assign('name',$name);
 			$this->assign('phone',$phone);
+			$this->assign('fax',$fax);
 			$this->assign('street',$street);
 			$this->assign('zipcode',$zipcode);
 			$this->assign('origin',$origin);
 			$this->assign('status',$status);
 			$this->assign('state',$state);
 			$this->assign('practice',$practice);
-			$this->assign('education',$education);
-			$this->assign('associations',$associations);
+			if(!empty($education)){
+				$this->assign('education',$education);
+			}
+			if(!empty($associations)){
+				$this->assign('associations',$associations);
+			}
+			if(!empty($awards)){
+				$this->assign('awards',$awards);
+			}
+			if(!empty($work_experience)){
+				$this->assign('work_experience',$work_experience);
+			}
+			if(!empty($legal_cases)){
+				$this->assign('legal_cases',$legal_cases);
+			}
+			if(!empty($publications)){
+				$this->assign('publications',$publications);
+			}
+			if(!empty($speaking_engagements)){
+				$this->assign('speaking_engagements',$speaking_engagements);
+			}
 			return $this->fetch();
 		}else{
 			return $this->error("未查询到用户信息！");
