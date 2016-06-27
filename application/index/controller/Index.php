db5eb770f6b5ddb4f914c62f94f7621b61ff938f
wanghaiming <18801116541@163.com>
2016-06-27 08:02:47 +0800

    Signed-off-by: wanghaiming <18801116541@163.com>

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -22,15 +22,60 @@ class Index extends Controller
 		$mongo = new Mongodb('avvo_lawyer_info','lawyers');
 		$findone = $mongo -> find($search,$cond);
 		foreach($findone as $key => $val){
+		var_dump($val);exit;
 			$user = $val['name'];
-			$city = $val['contact']['address']['city'];
-			$origin = $val['licenses']['0']['origin'];
+			if(!empty($val['contact']['address']['city'])){
+				$city = $val['contact']['address']['city'];
+			}else{
+				$city = "";
+			}
+			if(!empty($val['contact']['address']['name'])){
+				$name = $val['contact']['address']['name'];
+			}else{
+				$name = "";
+			}
+			if(!empty($val['contact']['phone'])){
+				$phone = $val['contact']['phone'];
+			}else{
+				$phone = "";
+			}
+			if(!empty($val['contact']['address']['street_address'])){
+				$street = $val['contact']['address']['street_address'];
+			}else{
+				$street = "";
+			}
+			if(!empty($val['contact']['address']['zipcode'])){
+				$zipcode = $val['contact']['address']['zipcode'];
+			}else{
+				$zipcode = "";
+			}
+			if(!empty($val['licenses']['0']['origin'])){
+				$origin = $val['licenses']['0']['origin'];
+			}else{
+				$origin = "";
+			}
+			if(!empty($val['licenses']['0']['status'])){
+				$status = $val['licenses']['0']['status'];
+			}else{
+				$status = "";
+			}
+			if(!empty($val['licenses']['0']['state'])){
+				$state = $val['licenses']['0']['state'];
+			}else{
+				$state = "";
+			}
 			$practice = implode($val['practice areas'],',');
 		}
 		if(!empty($user)){
 			$this->assign('user',$user);
 			$this->assign('city',$city);
+			$this->assign('name',$name);
+			$this->assign('phone',$phone);
+			$this->assign('street',$street);
+			$this->assign('zipcode',$zipcode);
 			$this->assign('origin',$origin);
+			$this->assign('status',$status);
+			$this->assign('state',$state);
 			$this->assign('practice',$practice);
 			return $this->fetch();
 		}else{
