7733e416658e6fec46110e8ec3d25424bf29c291
Cola Kang <colakang@gmail.com>
2016-07-12 23:58:40 +0000

    new search function & new ui

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -161,17 +161,162 @@ class Index extends Controller
 		$param3 = isset($_GET['param3'])?$_GET['param3']:"";
 		$page = isset($_GET['page'])?$_GET['page']:"1";		//Current page number;
 		$views = isset($_GET['views'])?$_GET['views']:"20";	//Deafult views = 10
+		$param2 = str_replace("+",".*",$param2);
+		$param2 = str_replace(" ",".*",$param2);
+		$param2 = str_replace("%20",".*",$param2);
 		$mongo  = new Mongodb('avvo_lawyer_info','lawyers');
 		switch($page)
 		{
 			case "1":
-				$query = array("{$param1}" => new \MongoRegex("/{$param2}.*/i"),"contact.address.state" =>new \MongoRegex("/{$param3}.*/i"));
+//				$query = array("{$param1}" => new \MongoRegex("/{$param2}.*/i"),"contact.address.state" =>new \MongoRegex("/{$param3}.*/i"));
+				$query = array(
+						'$and'=>array(
+								array(
+									'$or'=>array(
+										array("name" => new \MongoRegex("/{$param2}.*/i")),
+										array("practice areas" => new \MongoRegex("/{$param2}.*/i"))
+									)
+								),
+								array(
+									'$or'=>array(
+										array("contact.address.state" => new \MongoRegex("/{$param3}.*/i")),
+										array("contact.address.city" => new \MongoRegex("/{$param3}.*/i"))
+									),
+								),
+						)
+					);
+
 				$lists = $mongo->collection->find($query)->sort(array('avvo_id'=>1))->limit($views);
 				$count = $mongo->collection->find($query)->count();
 				break;
 			default:
 				$skip = ($page-1)*$views+1;
-				$query = array("{$param1}" => new \MongoRegex("/{$param2}.*/i"),"contact.address.state" =>new \MongoRegex("/{$param3}.*/i"));
+//				$query = array("{$param1}" => new \MongoRegex("/{$param2}.*/i"),"contact.address.state" =>new \MongoRegex("/{$param3}.*/i"));
+				$query = array(
+						'$and'=>array(
+								array(
+									'$or'=>array(
+										array("name" => new \MongoRegex("/{$param2}.*/i")),
+										array("practice areas" => new \MongoRegex("/{$param2}.*/i"))
+									)
+								),
+								array(
+									'$or'=>array(
+										array("contact.address.state" => new \MongoRegex("/{$param3}.*/i")),
+										array("contact.address.city" => new \MongoRegex("/{$param3}.*/i"))
+									),
+								),
+						)
+					);
+
+				$lists = $mongo->collection->find($query)->sort(array('avvo_id'=>1))->limit($views)->skip($skip);
+				$count = $mongo->collection->find($query)->count();
+				break;
+		}
+		foreach($lists as $k => $v){
+			$list[$k]['nyis_id'] = $v['avvo_id'];
+			$list[$k]['name']    = $v['name'];
+			if(!empty($v['contact']['phone'])){
+				$list[$k]['phone']   = $v['contact']['phone'];
+			}else{
+				$list[$k]['phone'] = "";
+			}
+			if(!empty($v['contact']['address']['city'])){
+				$list[$k]['city'] = $v['contact']['address']['city'];
+			}else{
+				$list[$k]['city'] = "";
+			}
+			if(!empty($v['contact']['address']['state'])){
+				$list[$k]['state'] = $v['contact']['address']['state'];
+			}else{
+				$list[$k]['state'] = "";
+			}
+			if(!empty($v['contact']['address']['zipcode'])){
+				$list[$k]['zipcode'] = $v['contact']['address']['zipcode'];
+			}else{
+				$list[$k]['zipcode'] = "";
+			}
+			if(!empty($v['practice areas'])){
+				$list[$k]['practice'] = $v['practice areas'];
+			}else{
+				$list[$k]['practice'] = "";
+			}
+		}
+		if(!empty($list)){
+			arsort($list);
+			$this->assign('list',$list);
+			$this->assign('param2',$param2);
+			$this->assign('param3',$param3);
+			$this->assign('param1',$param1);
+			if(!empty($count)){
+				$this->assign('count',$count);
+			}
+			if(!empty($page)){
+				$this->assign('page',$page);
+			}
+			$this->assign('debug',0);	//Show Debug Info 1=true,0=false;
+			$this->assign('pages',ceil($count/$views));	//Total pages;
+			$this->assign('views',$views);	//;
+		}else{
+			return $this->error("未查询到相关律师信息！");
+		}
+		return $this->fetch();
+	}
+
+	public function test()
+	{
+		$param1 = isset($_GET['param1'])?$_GET['param1']:"name";
+		$param2 = isset($_GET['param2'])?$_GET['param2']:"";
+		$param3 = isset($_GET['param3'])?$_GET['param3']:"";
+		$page = isset($_GET['page'])?$_GET['page']:"1";		//Current page number;
+		$views = isset($_GET['views'])?$_GET['views']:"20";	//Deafult views = 10
+		$mongo  = new Mongodb('avvo_lawyer_info','lawyers');
+		$param2 = str_replace("+",".*",$param2);
+		$param2 = str_replace(" ",".*",$param2);
+		$param2 = str_replace("%20",".*",$param2);
+		switch($page)
+		{
+			case "1":
+			//	$query = array("{$param1}" => new \MongoRegex("/{$param2}.*/i"),"contact.address.state" =>new \MongoRegex("/{$param3}.*/i"));
+
+				$query = array(
+						'$and'=>array(
+								array(
+									'$or'=>array(
+										array("name" => new \MongoRegex("/{$param2}.*/i")),
+										array("practice areas" => new \MongoRegex("/{$param2}.*/i"))
+									)
+								),
+								array(
+									'$or'=>array(
+										array("contact.address.state" => new \MongoRegex("/{$param3}.*/i")),
+										array("contact.address.city" => new \MongoRegex("/{$param3}.*/i"))
+									),
+								),
+						)
+					);
+
+				$lists = $mongo->collection->find($query)->sort(array('avvo_id'=>1))->limit($views);
+				$count = $mongo->collection->find($query)->count();
+				break;
+			default:
+				$skip = ($page-1)*$views+1;
+				$query = array(
+						'$and'=>array(
+								array(
+									'$or'=>array(
+										array("name" => new \MongoRegex("/{$param2}.*/i")),
+										array("practice areas" => new \MongoRegex("/{$param2}.*/i"))
+									)
+								),
+								array(
+									'$or'=>array(
+										array("contact.address.state" => new \MongoRegex("/{$param3}.*/i")),
+										array("contact.address.city" => new \MongoRegex("/{$param3}.*/i"))
+									),
+								),
+						)
+					);
 				$lists = $mongo->collection->find($query)->sort(array('avvo_id'=>1))->limit($views)->skip($skip);
 				$count = $mongo->collection->find($query)->count();
 				break;
@@ -223,6 +368,9 @@ class Index extends Controller
 		}else{
 			return $this->error("未查询到用户信息！");
 		}
-		return $this->fetch();
+		//return $this->fetch();
+		return json_encode($list);
 	}
+
+
 }
